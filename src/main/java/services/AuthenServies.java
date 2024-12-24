a0b0d2b45bf134d06cfb39ce860f300d016a3f93
HungLee104 <22130093@st.hcmuaf.edu.vn>
2024-12-24 22:37:28 +0700

    create authen services

---
+++ src/main/java/services/AuthenServies.java
@@ -0,0 +1,20 @@
+package services;
+
+import dao.AccountDao;
+import dao.UserDao;
+import models.AccountUser;
+import models.User;
+
+public class AuthenServies {
+    public User checkLogin(String username, String password) {
+        AccountDao accDao = new AccountDao();
+        AccountUser acc = accDao.findByUsername(username);
+        User user = null;
+        if (acc == null) return null;
+        if (acc!=null && password!=null
+                && acc.getPassword().equals(password)) {
+            user = acc.getUser();
+        }
+        return user;
+    }
+}
