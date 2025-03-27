62cfd7ccf617f5a43164785e8c64ebaba3fd5fe6
leedhung <lehung.bh141@gmail.com>
2025-03-27 09:29:04 +0700

    f commit

---
+++ LearnSpace/learn_space/learn_space/src/main/java/com/example/learn_space/LearnSpaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.learn_space;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpaceApplication.class, args);
+	}
+
+}
---
+++ LearnSpace/learn_space/learn_space/src/test/java/com/example/learn_space/LearnSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learn_space;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
